aa2f1849a66f9d8166b487c7814017a4382526d1
Julian Barg <jbarg2@uwo.ca>
2018-07-04 15:38:39 -0400

    Fixed variable naming.

--- R/build_NA.R
+++ R/build_NA.R
@@ -7,8 +7,8 @@
 build_NA <- function(df){
   name <- deparse(substitute(df))
   na_count <- data.frame(sapply(df, function(y) sum(is.na(y))))
-  na_count$table <- name
-  na_count$dataframe <- rownames(na_count)
+  na_count$df <- name
+  na_count$variable <- rownames(na_count)
   na_count['NAs'] <- na_count[1]
   na_count <- na_count[,-1]
   na_count$observations <- nrow(df)
