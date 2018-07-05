ba251c2263e336d823039175fe541f0fbc7668ea
Julian Barg <jbarg2@uwo.ca>
2018-07-05 17:06:31 -0400

    Revert "Extended testing."
    This reverts commit 4eace16fe3d2c93698b1748c4811aa605958740a.

--- tests/testthat/test-myFunction.R
+++ tests/testthat/test-myFunction.R
@@ -1,11 +1,6 @@
-a <- c(1, 2, 3)
-b <- c(4, NA, 6)
-c <- c(7, 8, 9)
-df <- data.frame(a, b, c)
-build_NA(df)
-
-d <- c(1, 2, 3)
-build_NA(d)
-
-df2 <- data.frame()
-build_NA(df2)
+a = c(1, 2, 3)
+b = c(4, NA, 6)
+c = c(7, 8, 9)
+df = data.frame(a, b, c)
+test = build_NA(df)
+head(test)
