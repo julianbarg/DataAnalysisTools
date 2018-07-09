2d3ff6b25f0d21f98b4d7b33b326dd110874a7f8
Julian Barg <jbarg2@uwo.ca>
2018-07-08 23:52:09 -0400

    Improved error handling. Guesses expected output for incorrect input.

--- R/build_NA.R
+++ R/build_NA.R
@@ -1,17 +1,78 @@
 #' Provide statistics on misssing data in dataframes.
 #'
-#' @param df Any non-empty dataframe.
-#' @return A dataframe with multiple rows of statistics on the input dataframe. The layout is such that mutliple of the returned dataframes can be concatenated to give a quick overview over the missing data in multiple dataframes. Function returns name of the dataframe, and for each variable (column) the number of NAs, total number of observations (rows), and percentage of missing data. Missing data must be formated to R NA values.  \code{x}
+#' @param x Any onempty data.frame.
+#' @return A dataframe with multiple rows of statistics on the input dataframe. The output is designed so that that mutliple of the returned dataframes can be concatenated to give a quick overview over the missing data in multiple dataframes. Function returns name of the dataframe, and for each variable (column) the number of NAs, total number of observations (rows), and percentage of missing data. Missing data must be formated to R NA values.  \code{x}
 #' @export
 
-build_NA <- function(df){
-  name <- deparse(substitute(df))
-  na_count <- data.frame(sapply(df, function(y) sum(is.na(y))))
-  na_count$df <- name
-  na_count$variable <- rownames(na_count)
-  na_count['NAs'] <- na_count[1]
-  na_count <- na_count[,-1]
-  na_count$observations <- nrow(df)
-  na_count$perc <- na_count$NAs / na_count$observations
-  return(na_count)
+build_NA <- function(x) {
+  ## First line is easy way to grab the name of the input, which we will denote
+  ## in the "source" column of the output.
+  input_name <- deparse(substitute(x))
+  output <- data.frame(matrix(ncol = 5, nrow = 0))
+  colnames(output) <- c("source", "variable", "NAs", "observations",
+                        "perc_NA")
+
+  ## Extected input is type data.frame. For performence reasons, we test for and
+  ## generate output for data.frame first.
+  if (is.data.frame(x)) {
+    if (nrow(x) == 0) {
+      warning("Input must be data.frame of length > 0. Output is empty")
+    } else {
+      output[1:ncol(x), ] <- NA
+      output$source <- input_name
+      output$variable <- names(x)
+      output$NAs <- sapply(x, function(y)
+        sum(is.na(y)))
+      output$observations <- nrow(x)
+      output$perc_NA <- output$NAs / output$observations
+    }
+  }
+  
+  ## For convenience sake, code also generates output for other data types, but
+  ## generates warning.
+  if (!is.data.frame(x)) {
+    if (is.vector(x) | is.factor(x) | is.list(x)) {
+      warning("Input must data.frame. Output might not be meaningful.")
+      output[1, ] <- NA
+      output$source <- input_name
+      output$variable <- input_name
+      output$NAs <- sum(is.na(x))
+      output$observations <- length(x)
+      output$perc_NA <- output$NAs / output$observations
+    }
+  
+    else if (is.matrix(x)) {
+      warning("Input must data.frame. Output might not be meaningful.")
+      output[1:ncol(x), ] <- NA
+      output$source <- input_name
+      output$variable <- if (!is.null(rownames(x))) {
+        rownames(x)
+      } else {
+        1:nrow(x)
+      }
+      output$NAs <- apply(x, 1, function(y) sum(is.na(y)))
+      output$observations <- nrow(x)
+      output$perc_NA <- output$NAs / output$observations  
+    }
+  
+    ## A matrix is also atomic. Matrix class is covered above. Therefore, we use
+    ## else if.
+    else if (is.atomic(x) & !is.null(x)) {
+      warning("Input must data.frame. Output might not be meaningful.")
+      output[1, ] <- NA
+      output$source <- input_name
+      output$variable <- input_name
+      output$NAs <- sum(is.na(x))
+      output$observations <- 1
+      output$perc_NA <- output$NAs / output$observations
+    }
+  
+    ## For other cases, throw meaningful warning and return empty output. Function
+    ## already provided a more specific warning for empty data.frame as input.
+    else {
+      warning("Input must be of type data.frame. Output is empty.")
+    }
+  }
+  
+  output
 }
