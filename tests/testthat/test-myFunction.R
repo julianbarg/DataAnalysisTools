5f90efe0634602cd283fdb40c28f4199d17b35c2
Julian Barg <jbarg2@uwo.ca>
2018-07-09 09:39:20 -0400

    Extended testing.

--- tests/testthat/test-myFunction.R
+++ tests/testthat/test-myFunction.R
@@ -1,6 +1,19 @@
-a = c(1, 2, 3)
-b = c(4, NA, 6)
-c = c(7, 8, 9)
-df = data.frame(a, b, c)
-test = build_NA(df)
-head(test)
+data("mtcars")
+build_NA(mtcars)
+
+gender <- as.factor(sample(c("m", "f"), 100, replace = TRUE))
+build_NA(gender)
+
+empty_dataframe <- data.frame()
+build_NA(empty_dataframe)
+
+m <- matrix(1:4, nrow = 2)
+build_NA(m)
+rownames(m) <- c("row1", "row2")
+build_NA(m)
+
+atomic <- 5
+build_NA(atomic)
+
+N <- NA
+build_NA(N)
