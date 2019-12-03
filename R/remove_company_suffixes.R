c20c8e3469cf97717cd4c6e96f1ab6ec022380c6
julianbarg <barg.julian@gmail.com>
2019-12-03 11:45:07 -0500

    Fixed and error and added more company suffixes to be removed.

--- R/remove_company_suffixes.R
+++ R/remove_company_suffixes.R
@@ -9,17 +9,21 @@
 #' @examples
 #' remove_company_suffixes(c("Enterprise Products Operating LLC", "Mercedes-Benz AG"))
 remove_company_suffixes <- function(string) {
-  company_suffixes <- c("LLC", "AG", "A.G.", "L.P.", "LP", "CORP", "CO", "INC.", "CO.")
+  company_suffixes <- c("LLC", "AG", "A.G.", "L.P.", "LP", "CORP", "CO", "INC.", "CO.", 
+                        "INC", "L.L.C.", "THE", "L.P", "LLP", "LLC.", "II", "L. P.")
   
   company_suffixes <- c(stringr::str_to_upper(company_suffixes), 
                         stringr::str_to_lower(company_suffixes), 
                         stringr::str_to_title(company_suffixes))
   # Remove either if space before and after, or if space before and end of string ($) after.
   in_end <- sapply(company_suffixes, function(x) paste0(" ", x, "$"), USE.NAMES = FALSE)
-  pattern <- paste0(c(in_end), collapse = "|")
+  after_comma <- sapply(company_suffixes, function(x) paste0(",", x, "$"), USE.NAMES = FALSE)
+  pattern <- paste0(c(in_end, after_comma), collapse = "|")
   
   wo_suffix <- stringr::str_remove_all(string, pattern = pattern)
+  wo_suffix <- stringr::str_remove(wo_suffix, ",$")
   # Run a second time, for combinations of company endings.
-  wo_suffix <- stringr::str_remove_all(string, pattern = pattern)
-  return(stringr::str_remove(wo_suffix, ",$"))
+  wo_suffix <- stringr::str_remove_all(wo_suffix, pattern = pattern)
+  wo_suffix <- stringr::str_remove(wo_suffix, ",$")
+  return(wo_suffix)
 }
